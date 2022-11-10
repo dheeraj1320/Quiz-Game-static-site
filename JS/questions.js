207d340c147182744e47f43c217e4211c7c6ea26
Dheeraj Kumar Manwani <dheerajmanwani2000@gmail.com>
2022-11-10 17:12:15 +0530

    question change

--- JS/questions.js
+++ JS/questions.js
@@ -1,91 +1,91 @@
-// const questions = [
-//   {
-//     numb: 1,
-//     selectedOptn: -1,
-//     question: "What is the capital of India?",
-//     answer: "New Delhi",
-//     options: ["Mumbai", "New Delhi", "Chennai", "Pune"],
-//   },
-//   {
-//     numb: 2,
-//     selectedOptn: -1,
-//     question: "What does CSS stand for?",
-//     answer: "Cascading Style Sheet",
-//     options: [
-//       "Common Style Sheet",
-//       "Colorful Style Sheet",
-//       "Computer Style Sheet",
-//       "Cascading Style Sheet",
-//     ],
-//   },
-//   {
-//     numb: 3,
-//     selectedOptn: -1,
-//     question: "What does HTML stand for?",
-//     answer: "Hyper Text Markup Language",
-//     options: [
-//       "Hyper Text Markup Language",
-//       "Hyper Text Preprocessor",
-//       "Hyper Text Multiple Language",
-//       "Hyper Language",
-//     ],
-//   },
-//   {
-//     numb: 4,
-//     selectedOptn: -1,
-//     question: "What does API stand for?",
-//     answer: "Application Programming Interface",
-//     options: [
-//       "Address Programming Interface",
-//       "Accessing Interface",
-//       "Address Programming Interface",
-//       "Application Programming Interface",
-//     ],
-//   },
-//   {
-//     numb: 5,
-//     selectedOptn: -1,
-//     question:
-//       "Which of the following keywords is not used to define a variable in Javascript?",
-//     answer: "int",
-//     options: ["var", "let", "int", "const"],
-//   },
-// ];
-
 const questions = [
   {
     numb: 1,
     selectedOptn: -1,
-    question: "Which of the following is a vector quantity?",
-    answer: "Acceleration",
-    options: ["Time", "Volume", "Acceleration", "Density"],
+    question: "What is the capital of India?",
+    answer: "New Delhi",
+    options: ["Mumbai", "New Delhi", "Chennai", "Pune"],
   },
   {
     numb: 2,
     selectedOptn: -1,
-    question: "Which of the following is an extensive property?",
-    answer: "Volume",
-    options: ["Volume", "Pressure", "Viscosity", "All of the above"],
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet",
+    ],
   },
   {
     numb: 3,
     selectedOptn: -1,
-    question: "An isobaric process, has constant _____",
-    answer: "Pressure",
-    options: ["Density", "Pressure", "Temperature", " Volume"],
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Markup Language",
+      "Hyper Text Preprocessor",
+      "Hyper Text Multiple Language",
+      "Hyper Language",
+    ],
   },
   {
     numb: 4,
     selectedOptn: -1,
-    question: "Deformation per unit length is called as ________",
-    answer: "Strain",
-    options: ["Modulus of Elasticity", "Stress", "Strain", "None of the above"],
+    question: "What does API stand for?",
+    answer: "Application Programming Interface",
+    options: [
+      "Address Programming Interface",
+      "Accessing Interface",
+      "Address Programming Interface",
+      "Application Programming Interface",
+    ],
   },
   {
     numb: 5,
     selectedOptn: -1,
-    question: "The S.I. unit of power is",
-    answer: "Watt",
-    options: ["Joule", "Pascal", "Newton", "Watt"],
+    question:
+      "Which of the following keywords is not used to define a variable in Javascript?",
+    answer: "int",
+    options: ["var", "let", "int", "const"],
   },
 ];
+
+// const questions = [
+//   {
+//     numb: 1,
+//     selectedOptn: -1,
+//     question: "Which of the following is a vector quantity?",
+//     answer: "Acceleration",
+//     options: ["Time", "Volume", "Acceleration", "Density"],
+//   },
+//   {
+//     numb: 2,
+//     selectedOptn: -1,
+//     question: "Which of the following is an extensive property?",
+//     answer: "Volume",
+//     options: ["Volume", "Pressure", "Viscosity", "All of the above"],
+//   },
+//   {
+//     numb: 3,
+//     selectedOptn: -1,
+//     question: "An isobaric process, has constant _____",
+//     answer: "Pressure",
+//     options: ["Density", "Pressure", "Temperature", " Volume"],
+//   },
+//   {
+//     numb: 4,
+//     selectedOptn: -1,
+//     question: "Deformation per unit length is called as ________",
+//     answer: "Strain",
+//     options: ["Modulus of Elasticity", "Stress", "Strain", "None of the above"],
+//   },
+//   {
+//     numb: 5,
+//     selectedOptn: -1,
+//     question: "The S.I. unit of power is",
+//     answer: "Watt",
+//     options: ["Joule", "Pascal", "Newton", "Watt"],
+//   },
+// ];
